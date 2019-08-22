2982418af712b39eec5ff18599a1317ea55c12b4
Chris <chris.anthony.yee@gmail.com>
2019-08-22 15:17:29 -0700

    added routing, controllers, and views

--- main-config.js
+++ main-config.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(_dirname, "..", "views");
 
 module.exports = {
   init(){
-
+  }
+  init(app, express){
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(_dirname, "..", "assets")));
   }
 };
--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,9 +5,10 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
 
   describe("GET /", () => {
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to the Message Board' in the body of the response", (done) => {
       request.get(base,(err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to the Message Board");
 
         done();
       });
